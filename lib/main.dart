7928faf73f87b16f653f3ee6a83f5bc65cb7714e
Mfundo Dhladhla <mfundod@contour.co.za>
2025-10-03 10:40:24 +0200

    Saved my work on main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -44,48 +44,136 @@ class MyAppState extends ChangeNotifier {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  var selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>(); //Keep track of Apps State at ALL TIMES
+
+    Widget page;
+    switch(selectedIndex){
+      case 0:
+        page = GeneratorPage();
+      case 1:
+        page = FavoritesPage();                 //A generic page-screen Widget that displays a view of an unfinished page
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+    }
+
+    return LayoutBuilder(                         // LayoutBuilder is used when constraints change (resize app, rotate phone, 1 widget grows in size thus effecting another's size)
+      builder: (context, constraints) {           // Add 'constraints' to parameters, to be used for new app view size
+        return Scaffold(                          //Every build() must return a Widget (Scaffold) to be viewed. You can add widgets under this (Expanded)
+          body: Row(
+            children: [
+              SafeArea(
+                child: NavigationRail(
+                  extended: constraints.maxWidth >= 600,
+                  destinations: [
+                    NavigationRailDestination(
+                      icon: Icon(Icons.home),
+                      label: Text('Home'),
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.favorite),
+                      label: Text('Favorites'),
+                    ),
+                  ],
+                  selectedIndex: selectedIndex,
+                  onDestinationSelected: (value) {
+                    setState(() {                       //Similar to notifyListener(), whatever change happens in the function will be updated on the view
+                      selectedIndex = value;
+                    });
+                  },
+                ),
+              ),
+              Expanded(                   //Greedy widget function. Takes as much of the remaining room as possible
+                child: Container(
+                  color: Theme.of(context).colorScheme.primaryContainer,
+                  child: page,
+                ),
+              ),
+            ],
+          ),
+        );
+      }
+    );
+  }
+}
+
+class GeneratorPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();     //Keep track of Apps State at ALL Times
     var pair = appState.current;
 
     IconData icon;
-    if(appState.favorites.contains(pair)){
+    if (appState.favorites.contains(pair)) {
       icon = Icons.favorite;
-    }else{
+    } else {
       icon = Icons.favorite_border;
     }
 
-    return Scaffold(                            //Every build() must return a Widget to be viewed
-      body: Center(                                     //Place column to be centered horizontally on Scaffold view
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,  //Place Content vertically center 
-          children: [
-            BigCard(pair: pair),
-            SizedBox(height: 10),               //An invisible box to create seperation
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                ElevatedButton.icon(
-                  onPressed: () {
-                    appState.toggleFavorite();
-                  },
-                  icon: Icon(icon),
-                  label: Text('Like'),
-                ),
-                SizedBox(width: 10,),
-                ElevatedButton(
-                  onPressed: () {
-                    appState.getNext();
-                  },
-                  child: Text('Next'),
-                ),
-              ],
-            )
-          ],
-        ),
+    return Center(                                  //Place column to be centered horizontally on View
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,  //Place content vertically center
+        children: [
+          BigCard(pair: pair),
+          SizedBox(height: 10),                       //An invisible box to create space
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleFavorite();
+                },
+                icon: Icon(icon),
+                label: Text('Like'),
+              ),
+              SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                },
+                child: Text('Next'),
+              ),
+            ],
+          ),
+        ],
       ),
+    );
+  }
+}
+
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    if (appState.favorites.isEmpty) {
+      return Center(
+        child: Text('No favorites yet.'),
+      );
+    }
+
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('You have '
+              '${appState.favorites.length} favorites:'),
+        ),
+        for (var pair in appState.favorites)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(pair.asLowerCase),
+          ),
+      ],
     );
   }
 }
